e96c444cb218d0dd841eeeff0a9d28f169336431
Wirawut99 <wirawut.che@spumail.net>
2024-11-08 00:41:16 +0700

    test

--- Project-Slide-Me/src/App.jsx
+++ Project-Slide-Me/src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css';
 import Addcar from './Addcar/Addcar';
 function App() {
-
-  return (
-    <div>
-      <h1>pobphon</h1>
-      <Addcar />
-    </div>
-  )
+	return (
+		<div>
+			<h1>dsfsds</h1>
+			<Addcar />
+		</div>
+	);
 }
 
 export default App;

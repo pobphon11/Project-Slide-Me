70e5b41f9322d44b3f4c87ba172ad69b52a47fcd
Wirawut99 <wirawut.che@spumail.net>
2024-11-07 21:55:40 +0700

    wirawut

--- Project-Slide-Me/src/App.jsx
+++ Project-Slide-Me/src/App.jsx
@@ -1,13 +1,7 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
-
-  return (
-    <div>
-      Test
-    </div>
-  )
+	return <div>wirawut</div>;
 }
 
-export default App
+export default App;

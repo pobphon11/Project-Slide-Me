57569920c4300735e4e1de30e1ae801628b04370
pobphon11 <pobphon.jit@spumail.net>
2024-11-09 02:19:15 +0700

    Hero-section v.1 not complete

--- Project-Slide-Me/src/components/Home-components/Hero-section/Hero.jsx
+++ Project-Slide-Me/src/components/Home-components/Hero-section/Hero.jsx
@@ -1,7 +1,20 @@
+import './Hero.css'
+
+
 function Hero() {
     return ( 
         <div className="Hero-container">
-            <h1>Slide Me</h1>
+           <div className="Hero-content">
+               <div className="Hero-text">
+               <h1>Slide Me</h1>
+               <p>No matter the job, <br />
+               we have the right vehicle for you. </p>
+               </div>
+               <div className="btn-Hero">
+                   <button className='btn btn-success  btn-xl ' style={{borderRadius: '50px' }} >Contact Me</button>
+               </div>
+               <div className="space-test"></div>
+           </div>
         </div>
      );
 }
